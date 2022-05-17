5c26cb691fd912095a8f7007e2733b3955272149
wjtelliott <wjtelliott@gmail.com>
2022-05-16 19:21:48 -0500

    Auth0 Initial

--- the-hood/src/App.js
+++ the-hood/src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginButton from './Components/LoginButton';
+import LogoutButton from './Components/LogoutButton';
+import Profile from './Components/Profile';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoginButton></LoginButton>
+      <LogoutButton></LogoutButton>
+      <p>Below is user info</p>
+      <Profile></Profile>
     </div>
   );
 }
