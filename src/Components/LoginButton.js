ef5e4e540c39507a046a11a0d9df1172cfedfca7
drfudd2001 <92401173+drfudd2001@users.noreply.github.com>
2022-05-21 13:25:58 -0500

    Update LoginButton.js

--- src/Components/LoginButton.js
+++ src/Components/LoginButton.js
@@ -4,7 +4,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 const LoginButton = () => {
     const { loginWithRedirect } = useAuth0();
     return (
-        <button onClick={() => loginWithRedirect()}>
+        <button className="btn btn-primary" onClick={() => loginWithRedirect()}>
             Log In / Sign Up
         </button>
     )
